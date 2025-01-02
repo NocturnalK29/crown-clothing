082970ef9aa73785d4fc1ee9117a18fb50101159
NocturnalK29 <kunalballal29@gmail.com>
2025-01-03 00:35:40 +0530

    fixing bugs(white screen)

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from 'react';
 
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 
